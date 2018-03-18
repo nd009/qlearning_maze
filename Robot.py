55b3842c1c929e71d4024ec2741cb4c675264c19
lolipopshock <xaviermassa@foxmail.com>
2018-03-19 01:05:52 +0800

    Major Update

---
+++ Robot.py
@@ -0,0 +1,120 @@
+import random
+
+class Robot(object):
+
+    def __init__(self, maze, alpha=0.5, gamma=0.9, epsilon0=0.5):
+
+        self.maze = maze
+        self.valid_actions = self.maze.valid_actions
+        self.state = None
+        self.action = None
+
+        # Set Parameters of the Learning Robot
+        self.alpha = alpha
+        self.gamma = gamma
+
+        self.epsilon0 = epsilon0
+        self.epsilon = epsilon0
+        self.t = 0
+
+        self.Qtable = {}
+        self.reset()
+
+    def reset(self):
+        """
+        Reset the robot
+        """
+        self.state = self.sense_state()
+        self.create_Qtable(self.state)
+
+    def set_status(self, learning=False, testing=False):
+        """
+        Determine whether the robot is learning its q table, or
+        exceuting the testing procedure.
+        """
+        self.learning = learning
+        self.testing = testing
+
+    def update_parameter(self):
+        """
+        Some of the paramters of the q learning robot can be altered,
+        update these parameters when necessary.
+        """
+        if self.testing:
+            # TODO 1. No random choice when testing
+            pass
+        else:
+            # TODO 2. Update parameters when learning
+            pass
+
+        return self.epsilon
+
+    def sense_state(self):
+        """
+        Get the current state of the robot. In this
+        """
+
+        # TODO 3. Return robot's current state
+        return None
+
+    def create_Qtable(self, state):
+        """
+        Create the qtable with the current state
+        """
+        # TODO 4. Create qtable with current state
+        # Our qtable should be a two level dict,
+        # Qtable[state] ={'u':xx, 'd':xx, ...}
+        pass
+
+    def choose_action(self):
+        """
+        Return an action according to given rules
+        """
+        def is_random_exploration():
+
+            # TODO 5. Return whether do random choice
+            # hint: generate a random number, and compare
+            # it with epsilon
+            pass
+
+        if self.learning:
+            if is_random_exploration():
+                # TODO 6. Return random choose aciton
+                return None
+            else:
+                # TODO 7. Return action with highest q value
+                return None
+        elif self.testing:
+            # TODO 7. choose action with highest q value
+        else:
+            # TODO 6. Return random choose aciton
+
+    def update_Qtable(self, r, action, next_state):
+        """
+        Update the qtable according to the given rule.
+        """
+        if self.learning:
+            pass
+            # TODO 8. When learning, update the q table accriding
+            # to the given rules
+
+    def update(self):
+        """
+        Describle the procedure what to do when update the robot.
+        Called every time in every epoch in training or testing.
+        Return current action and reward.
+        """
+        self.state = self.sense_state()
+        self.create_Qtable(self.state)
+
+        action = self.choose_action()
+        reward = self.maze.move_robot(action)
+
+        next_state = self.sense_state()
+        self.create_Qtable(next_state)
+
+        if self.learning and not self.testing:
+            self.update_Qtable(reward, action, next_state)
+            self.update_parameter()
+
+        return action, reward
